5fd9f4a1599a746e48f2cfeae74640faf162ac5c
Abdallah-Alwarawreh <Abooddevyt@outlook.com>
2024-11-25 16:23:06 +0300

    Fix currently active players in analytics

--- src/routes/dashboard/project/[id]/analytics/+page.server.ts
+++ src/routes/dashboard/project/[id]/analytics/+page.server.ts
@@ -87,9 +87,6 @@ export const load = (async ({ params, locals }) => {
         where: {
             projectId: params.id,
             endTime: null,
-            lastHeartbeat: {
-                gt: new Date(new Date().getTime() - 10 * 60 * 1000),
-            },
         },
     });
 
